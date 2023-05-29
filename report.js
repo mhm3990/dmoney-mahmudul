050254b1dc4e816d58784d5c339f26f3009fd5dc
mhm3990 <mahmudulmahadi7779@gmail.com>
2023-05-30 02:13:14 +0600

    Upload

---
+++ report.js
@@ -0,0 +1,16 @@
+const newman = require('newman');
+ 
+newman.run({
+    collection: require('./collection/dmoney.json'),
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
